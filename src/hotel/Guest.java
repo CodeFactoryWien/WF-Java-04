0a38cf623676ac280873811ddced53d61375d55e
drewitzmax <drewitz.max@googlemail.com>
2020-01-14 13:48:02 +0100

    Guest fields generated and basic constructor

--- src/hotel/Guest.java
+++ src/hotel/Guest.java
@@ -1,5 +1,30 @@
 package hotel;
 
+import java.sql.Date;
+
 public class Guest {
-    private String name;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String adress;
+    private int zipCode;
+    private String country;
+    private String phone;
+    private String email;
+    private int passportNumber;
+
+    public Guest(int id, String firstName, String lastName, Date birthDate, String adress, int zipCode,
+                 String country, String phone, String email, int passportNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.adress = adress;
+        this.zipCode = zipCode;
+        this.country = country;
+        this.phone = phone;
+        this.email = email;
+        this.passportNumber = passportNumber;
+    }
 }
