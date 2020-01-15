5eba85bbb4f36b44a5f7405d4aadf456480ef1bd
Steven Markhardt <steven.markhardt@gmx.at>
2020-01-15 12:31:48 +0100

    updated sql and typos

--- src/hotel/Guest.java
+++ src/hotel/Guest.java
@@ -1,26 +1,26 @@
-package Hotel;
+package hotel;
 
 import java.time.LocalDate;
 
-public class guest {
+public class Guest {
     private int id;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
-    private String adress;
+    private String address;
     private int zipCode;
     private String country;
     private String phone;
     private String email;
     private int passportNumber;
 
-    public guest(int id, String firstName, String lastName, LocalDate birthDate, String adress, int zipCode,
+    public Guest(int id, String firstName, String lastName, LocalDate birthDate, String address, int zipCode,
                  String country, String phone, String email, int passportNumber) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
-        this.adress = adress;
+        this.address = address;
         this.zipCode = zipCode;
         this.country = country;
         this.phone = phone;
