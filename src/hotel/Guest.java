6a54c756fef9eceb82bd615dca2f9ac2fe14c930
drewitzmax <drewitz.max@googlemail.com>
2020-01-16 11:26:03 +0100

    add getters , setters and constructors

--- src/hotel/Guest.java
+++ src/hotel/Guest.java
@@ -27,4 +27,57 @@ public class Guest {
         this.email = email;
         this.passportNumber = passportNumber;
     }
+    public Guest(String firstName, String lastName, LocalDate birthDate, String address, int zipCode,
+                 String country, String phone, String email, String passportNumber) {
+        this.id = 0;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.country = country;
+        this.phone = phone;
+        this.email = email;
+        this.passportNumber = passportNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
 }
