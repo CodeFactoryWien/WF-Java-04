886c277ba1b71495a5966ba7ee4646f21cdc4558
Steven Markhardt <steven.markhardt@gmx.at>
2020-01-16 14:42:12 +0100

    add semilogin & new database dump

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,6 +1,16 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
 public class MainController {
+
+    void start() throws Exception {
+        Stage S = new Stage();
+        S.setTitle("Hotel Managing Software");
+        S.setScene(FXMLLoader.load(getClass().getResource("/view/main.fxml")));
+        S.show();
+    }
 
     // Create new Guest //
     public void call_createGuestController() throws Exception {
