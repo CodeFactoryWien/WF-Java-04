4732a4f2b136a1d186d4cafe9f95a709c764de7f
drewitzmax <drewitz.max@googlemail.com>
2020-01-16 11:24:49 +0100

    removed firstFreeRoom

--- src/Hotel/Room.java
+++ src/Hotel/Room.java
@@ -25,27 +25,6 @@ public class Room {
         this.facilities = facilities;
     }
 
-    public static Room firstFreeRoom(String roomType, Date start, Date end){
-        try{
-            String query = "SELECT * FROM " +
-                    "(SELECT * FROM rooms INNER JOIN roomType ON rooms.fk_roomTypeID = roomType.roomTypeID " +
-                    "WHERE roomTypeName = \"" + roomType + "\") AS room " +
-                    "LEFT JOIN bookings ON bookings.fk_roomID = room.roomID " +
-                    "WHERE ((bookingFrom >'" + end +"' OR bookingUntil < '" + start + "') OR bookingFrom IS NULL)";
-            System.out.println(query);
-            ResultSet rs = Database.getData(query);
-            System.out.println(rs.first());
-            if(rs.first()){
-                System.out.println(rs.getString("roomTypeName"));
-                return new Room(rs.getInt("roomID"),rs.getString("roomTypeName"),rs.getDouble("roomTypePrice"),
-                        rs.getInt("roomTypeCapacity"),rs.getDouble("roomSize"),
-                        "",rs.getString("roomTypeFacilites"));
-            }
-        }catch(Exception e){
-            System.err.println(e);
-        }
-        return null;
-    }
 
     public String getType() {
         return type;
