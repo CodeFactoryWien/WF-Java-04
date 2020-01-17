651f8dd86fdb79f77ec2dc3271732a9193e860ae
roman-cf <roman.springer@outlook.com>
2020-01-17 14:29:43 +0100

    mit constructor

--- src/hotel/services/Wellness.java
+++ src/hotel/services/Wellness.java
@@ -1,4 +1,24 @@
 package hotel.services;
 
 public class Wellness {
+    int wellnessID;
+    String wellnessName;
+    String wellnessDescription;
+    Double wellnessPrice;
+
+    public Wellness(int wellnessID, String wellnessName, String wellnessDescription, Double wellnessPrice) {
+        this.wellnessID = wellnessID;
+        this.wellnessName = wellnessName;
+        this.wellnessDescription = wellnessDescription;
+        this.wellnessPrice = wellnessPrice;
+    }
+
+    @Override
+    public String toString() {
+        return wellnessName + " -- " + wellnessPrice;
+    }
+
+    public int getWellnessID() {
+        return wellnessID;
+    }
 }
