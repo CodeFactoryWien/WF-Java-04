81be4624862a1cfef7d191c3288c4763659589b2
roman-cf <roman.springer@outlook.com>
2020-01-22 14:11:37 +0100

    invoice funktioniert

--- src/controller/CreateInvoiceController.java
+++ src/controller/CreateInvoiceController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,7 +21,7 @@ import java.util.concurrent.TimeUnit;
 
 public class CreateInvoiceController {
     @FXML
-    private Label lblRoomNr, lblGuestName, lblQuantNights, lblQuantServices;
+    private Label lblRoomNr, lblGuestName, lblQuantNights, lblQuantServices, lblTotal, lblTax;
     @FXML
     private Button btnEditGuest, btnCreateInvoice, btnCancel;
     @FXML
@@ -35,8 +36,9 @@ public class CreateInvoiceController {
 
     private int bookingID;
     private Date today;
-    private long nights;
-    private ObservableList servicesInvoiceList;
+    private int nights;
+    private int total;
+    private ObservableList<InvoiceTable> servicesInvoiceList;
 
 
     void start() throws Exception {
@@ -52,36 +54,44 @@ public class CreateInvoiceController {
         try {
             bookingID = MainController.getBookingID();
             today = new Date(new java.util.Date().getTime());
-            getData();
+            getAndProvideData();
             initInvoiceTable();
-            // get guest infos
-            // get nights and price
-            // gett extras services
+
         }catch (Exception e){
             System.err.println(" Exception in initialize ");
         }
     }
 
-    public void getData()throws Exception{
+    public void getAndProvideData()throws Exception{
         try {
             PreparedStatement preparedStatementBooking =
-                    Database.c.prepareStatement("SELECT fk_roomID, firstName, lastName, checkedIn \n" +
-                            "FROM bookings\n" +
-                            "INNER JOIN guests ON bookings.fk_guestID = guests.guestID\n" +
-                            "WHERE bookingID = " + bookingID);
-            ResultSet rsHead = preparedStatementBooking.executeQuery();
-            while (rsHead.next()) {
-                int roomID = rsHead.getInt("fk_roomID");
-                lblRoomNr.setText(String.valueOf(roomID));
-                String firstName = rsHead.getString("firstName");
-                String lastName = rsHead.getString("lastName");
-                Date arrival = rsHead.getDate("checkedIn");
+                    Database.c.prepareStatement("SELECT fk_roomID, firstName, lastName, checkedIn, openAmount, roomtype.roomTypeName \n" +
+                            " FROM bookings\n" +
+                            " INNER JOIN guests ON bookings.fk_guestID = guests.guestID\n" +
+                            " INNER JOIN rooms on bookings.fk_roomID = rooms.roomID\n" +
+                            " INNER JOIN roomtype ON rooms.fk_roomTypeID = roomtype.roomTypeID\n" +
+                            " WHERE bookingID = " + bookingID);
+            ResultSet rsInvoiceBookings = preparedStatementBooking.executeQuery();
+            servicesInvoiceList = FXCollections.observableArrayList();
+            while (rsInvoiceBookings.next()) {
+                int roomID = rsInvoiceBookings.getInt("fk_roomID");
+                String firstName = rsInvoiceBookings.getString("firstName");
+                String lastName = rsInvoiceBookings.getString("lastName");
+                Date arrival = rsInvoiceBookings.getDate("checkedIn");
+                int ppn = rsInvoiceBookings.getInt("openAmount");
+                String roomType = rsInvoiceBookings.getString("roomTypeName");
                 Date date1 = arrival;
                 Date date2 = today;
                 long diff = date2.getTime() - date1.getTime();
-                nights = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+                long lngNights = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+                nights = new BigDecimal(lngNights).intValueExact();
+                System.out.println(nights + " "+ ppn);
+                servicesInvoiceList.add(new InvoiceTable(roomID,arrival.toString(),nights," ",roomType,ppn,nights*ppn));
+
+                lblRoomNr.setText(String.valueOf(roomID));
                 lblGuestName.setText(firstName + " " + lastName);
                 lblQuantNights.setText(String.valueOf(nights));
+                total=total+(nights*ppn);
             }
             PreparedStatement preparedStatementServices =
                     Database.c.prepareStatement("SELECT servicesID, serviceType, serviceDate, \n" +
@@ -96,7 +106,7 @@ public class CreateInvoiceController {
                             " GROUP BY serv_minibar.mbItem, serv_movies.movieName, serv_wellness.wellnessName\n" +
                             " HAVING COUNT(*) > 0");
             ResultSet rsInvoiceServices = preparedStatementServices.executeQuery();
-            servicesInvoiceList = FXCollections.observableArrayList();
+
             while (rsInvoiceServices.next()){
                 int i = rsInvoiceServices.getInt("servicesID");
                 String serviceType = rsInvoiceServices.getString("serviceType");
@@ -105,8 +115,10 @@ public class CreateInvoiceController {
                 int serviceQuant = rsInvoiceServices.getInt("quant");
                 String serviceName = rsInvoiceServices.getString("serviceName");
                 servicesInvoiceList.add(new InvoiceTable(i,serviceDate,serviceQuant,serviceType,serviceName,servicePPS,servicePPS*serviceQuant));
+                total = total+(servicePPS*serviceQuant);
             }
-
+            lblTotal.setText(String.valueOf(total));
+            lblTax.setText(String.valueOf(total-(total*0.9)));
         }catch (Exception e){
             System.out.println("getdata exption");
         }
@@ -115,18 +127,19 @@ public class CreateInvoiceController {
 
     public void initInvoiceTable()throws Exception{
         try {
-                columnID.setCellValueFactory(new PropertyValueFactory<>("servicesID"));
-                columnDate.setCellValueFactory(new PropertyValueFactory<>("serviceDate"));
-                columnQuant.setCellValueFactory(new PropertyValueFactory<>("quant"));
-                columnType.setCellValueFactory(new PropertyValueFactory<>("serviceType"));
-                columnName.setCellValueFactory(new PropertyValueFactory<>("serviceName"));
-                columnPPS.setCellValueFactory(new PropertyValueFactory<>("pps"));
-                columnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+            columnID.setCellValueFactory(new PropertyValueFactory<>("servicesID"));
+            columnDate.setCellValueFactory(new PropertyValueFactory<>("serviceDate"));
+            columnQuant.setCellValueFactory(new PropertyValueFactory<>("quant"));
+            columnType.setCellValueFactory(new PropertyValueFactory<>("serviceType"));
+            columnName.setCellValueFactory(new PropertyValueFactory<>("serviceName"));
+            columnPPS.setCellValueFactory(new PropertyValueFactory<>("pps"));
+            columnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
 
             tableInvoice.setItems(servicesInvoiceList);
 
         }catch (Exception e){
             System.out.println("Befüllen der Rechnungstabelle nicht möglich");
+            e.printStackTrace();
         }
     }
 
